c85706402fc9be2c8842f46227307f9e5392d2c1
Max Kohler <hello@maxkohler.com>
2025-04-22 15:27:07 +0200

    map ctrl-/ to toggle comment

--- init.lua
+++ init.lua
@@ -14,6 +14,7 @@ vim.cmd "nnoremap <silent><C-r> :NeovimProjectHistory<Return>"
 vim.cmd "nnoremap <silent><C-j> :10sp<Return><C-w>x<Return><C-w>j :term<Return>"
 vim.cmd "nnoremap <silent><C-P> :lua require'fzf-lua'.files({winopts={width=0.40, row=0, col=0, border='single'}, previewer=false, cmd='fd --exclude .git' })<Return>"
 vim.cmd "nnoremap <silent><C-z> :set wrap!<Return>"
+vim.cmd "nmap <C-.> gcc"
 vim.cmd "tnoremap <Esc> <C-\\><C-n>"
 
 vim.cmd "command Colors FzfLua colorschemes winopts.width=.15 winopts.row=0 winopts.col=1 winopts.border=single"
